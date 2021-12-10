15441cd6c00fea51839af3825590c86a79c8a91a
Damien Burke <damien@damienburke.com>
2021-12-10 13:46:57 -0800

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ module BadApples
   class Application < Rails::Application
     config.load_defaults 7.0
 
+    config.generators.controller_specs = true
+    config.generators.helper = false
+    config.generators.request_specs = false
+    config.generators.stylesheets = false
     config.generators.test_framework = :rspec
+    config.generators.view_specs = false
   end
 end
