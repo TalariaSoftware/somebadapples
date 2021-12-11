4ce1a3c2d3d4b50df52175e1847e2d7c2f113dc8
Damien Burke <damien@damienburke.com>
2021-12-10 22:58:18 -0800

    Add ability to manage incidents

---
+++ spec/system/incident_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "Incident", type: :system do
+  before do
+    driven_by(:rack_test)
+    sign_in user
+  end
+
+  let(:user) { create :user }
+  let(:officer) { create :officer, first_name: "Javert", last_name: "Valjean" }
+
+  scenario "Manage incident" do
+    visit officer_path(officer)
+    expect(page).to have_content("Javert Valjean")
+
+    click_on "Add incident"
+
+    fill_in "Description", with: "Beat driver"
+    fill_in "Datetime", with: "2021-12-10 23:00"
+    click_on "Create"
+
+    expect(page).to have_content("Javert Valjean")
+
+    expect(page).to have_content("Beat driver")
+    expect(page).to have_content("December 10, 2021")
+
+    click_on "Edit incident"
+
+    fill_in "Description", with: "Beat pedestrian"
+    fill_in "Datetime", with: "2021-12-09 23:00"
+    click_on "Update"
+
+    expect(page).to have_content("Beat pedestrian")
+    expect(page).to have_content("December 09, 2021")
+
+    click_on "Edit incident"
+    click_on "Delete"
+
+    expect(page).to have_content("Javert Valjean")
+    expect(page).not_to have_content("Dec")
+  end
+end
