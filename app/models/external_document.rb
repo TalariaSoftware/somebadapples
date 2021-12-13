35180a95577c71f09a01cdeb59b9bcd055fff34f
Damien Burke <damien@damienburke.com>
2021-12-13 15:39:21 -0800

    Add ExternalDocument#youtube?

--- app/models/external_document.rb
+++ app/models/external_document.rb
@@ -1,3 +1,10 @@
 class ExternalDocument < ApplicationRecord
   belongs_to :incident
+
+  def youtube?
+    domain = PublicSuffix.domain(URI.parse(url).host)
+    domain.in? ['youtube.com', 'youtu.be']
+  rescue URI::InvalidURIError
+    false
+  end
 end
--- spec/models/external_document_spec.rb
+++ spec/models/external_document_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ExternalDocument, type: :model do
   it { is_expected.to belong_to(:incident) }
+
+  describe '#youtube?' do
+    subject(:document) { build :external_document, url: url }
+
+    context 'when the host is not a YouTube domain' do
+      let(:url) { 'https://www.myspace.com/youtube.com' }
+
+      it { is_expected.not_to be_youtube }
+    end
+
+    context 'when the host is youtube.com' do
+      let(:url) { 'https://youtube.com/watch?v=6SFNW5F8K9Y' }
+
+      it { is_expected.to be_youtube }
+    end
+
+    context 'when the host is youtu.be' do
+      let(:url) { 'https://youtu.be/watch?v=6SFNW5F8K9Y' }
+
+      it { is_expected.to be_youtube }
+    end
+  end
 end
