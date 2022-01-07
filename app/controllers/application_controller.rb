814b4fc51d83e0ebf04a6d4d241c1373fbfe2d74
Damien Burke <damien@damienburke.com>
2022-01-07 11:08:35 -0800

    Add exception handling for unauthorized actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   include Pundit
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  def not_authorized(exception)
+    if current_user.present?
+      policy_name = exception.policy.class.to_s.underscore
+      flash[:error] = t("#{policy_name}.#{exception.query}", scope: :pundit,
+                default: :default)
+      redirect_back fallback_location: root_path, status: :see_other
+    else
+      flash[:notice] = t('pundit.not_logged_in')
+
+      if request.get? || request.head?
+        store_location_for(:user, request.path)
+      else
+        store_location_for(:user, request.referer)
+      end
+
+      redirect_to new_user_session_path, status: :see_other
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,102 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  include Rails.application.routes.url_helpers
+
+  context "when the user is unauthorized" do
+    controller do
+      def index
+        # council = FactoryBot.create :neighborhood_council # rubocop:disable RSpec/FactoryBot/SyntaxMethods
+        policy =  ExternalDocumentPolicy.new(current_user, nil)
+        raise Pundit::NotAuthorizedError.new(policy: policy, query: 'index')
+      end
+
+      def create
+        # council = FactoryBot.create :neighborhood_council # rubocop:disable RSpec/FactoryBot/SyntaxMethods
+        policy =  ExternalDocumentPolicy.new(current_user, nil)
+        raise Pundit::NotAuthorizedError.new(policy: policy, query: 'create')
+      end
+    end
+
+    context "when the user is logged in" do
+      controller do
+        def current_user
+          @current_user ||= FactoryBot.create :user # rubocop:disable RSpec/FactoryBot/SyntaxMethods
+        end
+      end
+
+      it "sets an error message" do
+        get :index
+        expect(flash[:error]).to eq("You are not authorized for that.")
+      end
+
+      it "redirects to a different resource" do
+        get :index
+        expect(response).to have_http_status(:see_other)
+      end
+
+      context "when there is no referrer" do
+        it "redirects to root" do
+          get :index
+          expect(response).to redirect_to('/')
+        end
+      end
+
+      context "when there is a local referrer" do
+        before { request.headers['Referer'] = 'http://test.host/orig_path' }
+
+        it "redirects to the referer" do
+          get :index
+          expect(response).to redirect_to('/orig_path')
+        end
+      end
+
+      context "when there is a referrer from another host" do
+        before { request.headers['Referer'] = 'http://naaga.org/orig_path' }
+
+        it "redirects to the root path" do
+          get :index
+          expect(response).to redirect_to('/')
+        end
+      end
+    end
+
+    context "when the user is not logged in" do
+      controller do
+        def current_user
+          nil
+        end
+      end
+      before { request.headers['Referer'] = 'http://test.host/orig_path' }
+
+      it "sets an notice message" do
+        get :index
+        expect(flash[:notice]).to eq("Sign in to continue.")
+      end
+
+      it "redirects to a different resource" do
+        get :index
+        expect(response).to have_http_status(:see_other)
+      end
+
+      it "redirects to the login page" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      context "when the request is a get" do
+        it "remembers the current path" do
+          get :index
+          expect(controller.stored_location_for(:user)).to eq('/anonymous')
+        end
+      end
+
+      context "when the request is not a get" do
+        it "remembers the referrer path" do
+          post :create
+          expect(controller.stored_location_for(:user)).to eq('/orig_path')
+        end
+      end
+    end
+  end
+end
