c64c74bbbf851ae6742d641578986c19aea07afb
Damien Burke <damien@damienburke.com>
2022-04-22 16:46:41 -0700

    Update gems

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,9 +1,9 @@
 Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
 
   config.enabled = false if Rails.env.test?
 
   # Override Rails env; on Heroku staging is a "production" environment
   # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
-  config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
+  config.environment = ENV.fetch('ROLLBAR_ENV', Rails.env)
 end
