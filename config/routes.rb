6b12e4b9f29b5d919cd5a42de38fd7d1d7a110cf
Damien Burke <damien@damienburke.com>
2023-08-14 16:46:42 -0500

    Remove routes for removed controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,6 @@ Rails.application.routes.draw do
   resources :incidents do
     resources :roles, only: %i[new create edit update destroy]
   end
-  resources :officers, only: %i[show new edit create update destroy] do
-    resources :positions, only: %i[new create edit update destroy]
-  end
-  resources :agencies do
-    resources :officers, only: %i[index]
-  end
 
   resources :post_records, only: %i[index]
   get '/post_records/:post_id', to: 'post_records#index', as: 'post_id'
